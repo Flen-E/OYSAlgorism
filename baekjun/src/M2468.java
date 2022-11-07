ac25c102397ba465e69e73886908b568453d96d0
Flen-E <cjj642@naver.com>
2022-11-08 01:35:31 +0900

    m2468

--- baekjun/src/M2468.java
+++ baekjun/src/M2468.java
@@ -1,40 +1,65 @@
 import java.util.Scanner;
 
 public class M2468 {
+    static int safeZone[][];
+    static int checked[][];
+    static int x[] = {1,0,-1,0};
+    static int y[] = {0,1,0,-1};
+    static int N;
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        int N = s.nextInt(); //nxn
-        int height[][] = new int[N+2][N+2];
+        N = s.nextInt(); //nxn
+        int height[][] = new int[N][N];
         int maxCount = 0;
 
-        for(int i = 1; i < N+1; i++){
-            for(int j = 1; j < N+1; j++){
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
                 height[i][j] = s.nextInt();
             }
         }
 
+
         for(int h = 1; h < 101; h++){
-            int safeZone[][] = new int[N][N];
-            int checked[][] = new int[N][N];
+            safeZone= new int[N][N];
+            checked= new int[N][N];
             int count = 0;
-            for(int i = 1; i < N+1; i++){
-                for(int j = 1; j < N+1; j++){
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
                    if(height[i][j] >=h)
                        safeZone[i][j] = 1;
                 }
             }
             for(int i = 0; i < N; i++){
                 for(int j = 0; j < N; j++){
+                    if(safeZone[i][j] ==1 && checked[i][j] == 0){
+                        Cal(i,j);
+                        count++;
+                    }
+                }
+            }
+            if(maxCount <count)
+                maxCount = count;
 
+        }
+        System.out.println(maxCount);
+
+    }
+    public static void Cal(int dotX,int dotY){
+        checked[dotX][dotY] =1;
+
+        for(int i = 0; i <4; i++){
+            int x1 = dotX +x[i];
+            int y1 = dotY +y[i];
+            if(x1 >=0 && y1>=0 && x1 <N && y1 <N){
+                if(safeZone[x1][y1] ==1 && checked[x1][y1] !=1){
+                    Cal(x1,y1);
                 }
             }
 
-
-
         }
-
     }
+
 
 
 }
